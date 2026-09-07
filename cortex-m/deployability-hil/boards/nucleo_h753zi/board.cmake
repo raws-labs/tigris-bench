803929ce39e4905f9044a7e177f1929b52b93459
asteinh <armin.steinhauser@raws.at>
2026-09-07 22:41:28 +0200

    consume tigris-cortex-m for board bring-up: platform.h shims onto the package HAL; boards source their port/linker from tigris-cortex-m

--- cortex-m/deployability-hil/boards/nucleo_h753zi/board.cmake
+++ cortex-m/deployability-hil/boards/nucleo_h753zi/board.cmake
@@ -13,10 +13,11 @@ set(BOARD_DEFINES STM32H753xx)
 
 set(BOARD_STARTUP       ${CMSIS_DEVICE_H7_DIR}/Source/Templates/gcc/startup_stm32h753xx.s)
 set(BOARD_SYSTEM        ${CMSIS_DEVICE_H7_DIR}/Source/Templates/system_stm32h7xx.c)
-set(BOARD_BSP           ${CMAKE_CURRENT_LIST_DIR}/bsp.c)
-set(BOARD_LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/STM32H753ZITx_FLASH.ld)
+set(BOARD_BSP           ${TIGRIS_CORTEX_M_ROOT}/src/hal/platform/nucleo_h753zi/tigris_hal_h753.c)
+set(BOARD_LINKER_SCRIPT ${TIGRIS_CORTEX_M_ROOT}/src/hal/platform/nucleo_h753zi/stm32h753zi.ld)
 
 set(BOARD_INCLUDES
+    ${TIGRIS_CORTEX_M_ROOT}/include
     ${CMSIS_CORE_DIR}/Include
     ${CMSIS_DEVICE_H7_DIR}/Include)
 
